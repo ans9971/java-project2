cac507cca5b6d2c1448708c1438e259aefe58ca8
ans9971 <ans9971@naver.com>
2023-04-19 14:47:10 +0900

    BufferedReaderEx

--- src/main/java/com/example/java_project2/week1/BufferedReaderEx.java
+++ src/main/java/com/example/java_project2/week1/BufferedReaderEx.java
@@ -8,6 +8,7 @@ public class BufferedReaderEx {
     public void readALine() throws IOException {
         InputStreamReader is = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(is);//카트가지고오기
+
 //        String line = br.readLine();
         System.out.println("line1 = " + br.readLine());
         System.out.println("line2 = " + br.readLine());
