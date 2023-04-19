026d37c79bcbfe615d2ccc73299d3521c7701c83
ans9971 <ans9971@naver.com>
2023-04-19 14:46:04 +0900

    readTwoChar

---
+++ src/main/java/com/example/java_project2/week1/BufferedReaderEx.java
@@ -0,0 +1,15 @@
+package com.example.java_project2.week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderEx {
+    public void readALine() throws IOException {
+        InputStreamReader is = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(is);//카트가지고오기
+//        String line = br.readLine();
+        System.out.println("line1 = " + br.readLine());
+        System.out.println("line2 = " + br.readLine());
+    }
+}
